6c2243bf27fc8a9df0c0d1290c86f03f9f777c57
Praiyon <praiyon@gmail.com>
2019-09-16 18:47:18 -0400

    not working on production

--- api-handler.js
+++ api-handler.js
@@ -1,61 +1,47 @@
 'use strict';
 const User = require('@pbnj-xintern/xintern-commons/models/User');
 const status = require('@pbnj-xintern/xintern-commons/util/status')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 var db = require('@pbnj-xintern/xintern-commons/util/db');
-const TEST_KEY = process.env.TEST_KEY
-
+const dbUrl = process.env.MONGO_URL;
 
 module.exports.createUser = async (event, context, callback) => {
-db(
-  'mongodb+srv://pbnj:test@cluster0-iuvsd.mongodb.net/test?retryWrites=true&w=majority', 
-  () => {
+  return db(dbUrl, () => 
     User.find({
       username: event.body.username
-    }).then(user => {
-      if (user.length > 0){
-        console.log("here")
-        return status.createErrorResponse(409, "Username exists");
-      } else{
-        const hash = bcrypt.hash(event.body.password, 10, (err, hash) => {
-          if (err) {
-              console.log(err)
-          } else {
-              const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                username: event.body.username,
-                password: hash,
-                email: event.body.email,
-                institution: event.body.institution,
-                name: {
-                    first: event.body.name.first,
-                    last: event.body.name.last
-                },
-                sex:  event.body.sex ,
-                program: event.body.program,
-                age: event.body.age,
-                isShowInfo: event.body.isShowInfo,
-                role: event.body.role, //XINT or ADMIN
-                profileLinks: event.body.profileLinks
-              });
-              db('mongodb+srv://pbnj:test@cluster0-iuvsd.mongodb.net/test?retryWrites=true&w=majority', 
-              () => {
-                user.save()
-                  .then(result => {
-                      console.log(result)
-                      return status.sendSuccessResponse(201, "success")
-                  })
-                  .catch(err => {
-                    console.log("here")
-                      console.log(err)
-                  })})
+    })
+  ).then(user => {
+    if (user.length > 0){
+      return status.createErrorResponse(401, "User exists");
+    }else{
+      const hash = bcrypt.hash(event.body.password, 10, (err, hash) => {
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          username: event.body.username,
+          password: hash,
+          name: {
+              first: event.body.name.first,
+              last: event.body.name.last
+          },
+          sex:  event.body.sex ,
+          program: event.body.program,
+          age: event.body.age,
+          isShowInfo: event.body.isShowInfo,
+          role: event.body.role, //XINT or ADMIN
+        });
+        return db(dbUrl, () => user.save().catch(err => 
+          status.createErrorResponse(500, err)
+        ))
+        .then(res => {
+          console.log(res, 'RESSSSS')
+          if (res){
+            return status.createSuccessResponse(201, "User created")
           }
+        })
       });
-      }
-      })
+    }
   })
-
 };
 
 
