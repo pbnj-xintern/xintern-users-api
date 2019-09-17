7eb97d8d1c7742a4d7fd27c2fbbe87ebb3800ac5
Praiyon <praiyon@gmail.com>
2019-09-17 13:18:41 -0400

    switched implementation to await/async

--- api-handler.js
+++ api-handler.js
@@ -5,43 +5,10 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 var db = require('@pbnj-xintern/xintern-commons/util/db');
 const dbUrl = process.env.MONGO_URL;
-
+const userCreator = require('./helpers/user');
 module.exports.createUser = async (event, context, callback) => {
-  return db(dbUrl, () => 
-    User.find({
-      username: event.body.username
-    })
-  ).then(user => {
-    if (user.length > 0){
-      return status.createErrorResponse(401, "User exists");
-    }else{
-      const hash = bcrypt.hash(event.body.password, 10, (err, hash) => {
-        const user = new User({
-          _id: new mongoose.Types.ObjectId(),
-          username: event.body.username,
-          password: hash,
-          name: {
-              first: event.body.name.first,
-              last: event.body.name.last
-          },
-          sex:  event.body.sex ,
-          program: event.body.program,
-          age: event.body.age,
-          isShowInfo: event.body.isShowInfo,
-          role: event.body.role, //XINT or ADMIN
-        });
-        return db(dbUrl, () => user.save().catch(err => 
-          status.createErrorResponse(500, err)
-        ))
-        .then(res => {
-          console.log(res, 'RESSSSS')
-          if (res){
-            return status.createSuccessResponse(201, "User created")
-          }
-        })
-      });
-    }
-  })
+  let result = await userCreator.createUser(event);
+  console.log(result)
 };
 
 
