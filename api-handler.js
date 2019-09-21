97ef1ea9fc240fbc7a7bebbc591d61217c4e9390
Praiyon <praiyon@gmail.com>
2019-09-21 16:26:13 -0400

    optimized imports

--- api-handler.js
+++ api-handler.js
@@ -1,12 +1,7 @@
 'use strict';
 const User = require('@pbnj-xintern/xintern-commons/models/User');
 const status = require('@pbnj-xintern/xintern-commons/util/status')
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-var db = require('@pbnj-xintern/xintern-commons/util/db');
-const dbUrl = process.env.MONGO_URL;
 const userHelper = require('./helpers/user');
-const checkRequest = require('@pbnj-xintern/xintern-commons/util/request_checker');
 const checkRequestBody = require('@pbnj-xintern/xintern-commons/util/request_checker');
 module.exports.createUser = async (event, context, callback) => {
 	return await 
